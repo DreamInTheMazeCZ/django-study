8536039aa0334eac75e1acaf7a589ca8be4ae907
Jihyun Choi <ditm@softzion.com>
2024-03-15 16:34:27 +0900

    최초 커밋

---
+++ members/views.py
@@ -0,0 +1,34 @@
+# from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
+
+def members(request):
+    # return HttpResponse("Hello World!")
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+        'mymems' : mymembers,
+    }
+    
+    return HttpResponse(template.render(context, request))
+
+def details(request, id_num):
+    mymember = Member.objects.get(id=id_num)
+    template = loader.get_template('details.html')
+    context = {
+        'mymem' : mymember,
+    }
+    
+    return HttpResponse(template.render(context, request))
+
+def main(request):
+    template = loader.get_template('main.html')
+    return HttpResponse(template.render())
+
+def testing(request):
+    template = loader.get_template('template.html')
+    context = {
+        'fruits' : ['Apple', 'Banana', 'Cherry']
+    }
+    return HttpResponse(template.render(context, request))
